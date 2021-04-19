0915df1443805756e90405b89413645b042f5676
domynique <dshelbyv@gmail.com>
2021-04-18 20:38:04 -0400

    push

---
+++ app.js
@@ -0,0 +1,54 @@
+
+var x = 10
+var y = 5 
+
+function addTwo(x, y){
+    return x + y;   
+}
+var result = add(x,y);
+console.log(10 + 5);
+
+
+function subTwo(x, y){
+    return x - y;  
+}
+var result = subtract(x,y);
+console.log(10 - 5);
+
+
+function multiTwo(x, y){
+    return x * y;
+}
+var result = multiply(x,y);
+console.log(10 * 5);
+
+
+function divTwo(x, y){
+    return x / y; 
+}
+var result = divide(x,y);
+ console.log (10 / 5);
+
+
+
+var sum = (x,y) => (x + y);
+console.log(sum(x,y));
+
+
+
+var result = (x,y) => (x - y);
+console.log(result(x,y));
+
+
+var result = (x,y) => (x * y);
+console.log(result(x,y));
+
+
+var result = (x,y) => (x / y);
+console.log(result(x,y));
+
+
+
+
+
+
